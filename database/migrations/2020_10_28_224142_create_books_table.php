e4366c1d58c47e933b7e2875e810aa35d8ac38e4
gl-guccione <g.guccione3@gmail.com>
2020-10-28 23:56:11 +0100

    Added table books -> database

---
+++ database/migrations/2020_10_28_224142_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn', 13)->unique();
+            $table->string('title', 20);
+            $table->string('author', 20);
+            $table->string('genre', 20);
+            $table->string('image');
+            $table->date('year');
+            $table->smallInteger('pages');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
