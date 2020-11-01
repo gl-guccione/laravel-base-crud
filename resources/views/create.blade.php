cb2e42ea57a2ef1254c3cf6d9de0e2aab269dd25
gl-guccione <g.guccione3@gmail.com>
2020-11-02 00:29:00 +0100

    Added sass for create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,30 +4,70 @@
 
 @section('mainContent')
 
-  <form action="{{route('books.store')}}" method="POST">
-    @csrf
-    @method('POST')
+  <main class="main_boxed">
 
-    <input type="text" name="title" placeholder="title" required>
-    <input type="text" name="image" placeholder="image" required>
-    <input type="text" name="isbn" placeholder="isbn" required>
-    <input type="text" name="author" placeholder="author" required>
-    <input type="text" name="genre" placeholder="genre" required>
-    <input type="date" name="year" placeholder="year" required>
-    <input type="number" name="pages" placeholder="pages" required>
+    <form action="{{route('books.store')}}" method="POST">
+      @csrf
+      @method('POST')
 
-    <input type="submit" value="Invia">
+      <div class="book-show">
 
-  </form>
+        <div class="book-show__right">
 
-  @if ($errors->any())
-      <div>
-          <ul>
-              @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-              @endforeach
-          </ul>
+          <div>
+            <h5 class="input-label">ISBN:</h5>
+            <input class="input-full" type="text" name="isbn" placeholder="isbn" required>
+          </div>
+
+          <div>
+            <h5 class="input-label">Titolo:</h5>
+            <input class="input-full" type="text" name="title" placeholder="title" required>
+          </div>
+
+          <div>
+            <h5 class="input-label">URL immagine:</h5>
+            <input class="input-full" type="text" name="image" placeholder="image" required>
+          </div>
+
+          <div>
+            <h5 class="input-label">Autore:</h5>
+            <input class="input-full" type="text" name="author" placeholder="author" required>
+          </div>
+
+          <div>
+            <h5 class="input-label">Genere:</h5>
+            <input class="input-full" type="text" name="genre" placeholder="genre" required>
+          </div>
+
+          <div>
+            <h5 class="input-label">Anno di pubblicazione:</h5>
+            <input class="input-full" type="date" name="year" placeholder="year" required>
+          </div>
+
+          <div>
+            <h5 class="input-label">Numero di pagine:</h5>
+            <input class="input-full" type="number" name="pages" placeholder="pages" required>
+          </div>
+
+        </div>
+
       </div>
-  @endif
+
+      <input class="btn ml_20" type="submit" value="Aggiungi nuovo libro">
+
+
+    </form>
+
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+  </main>
 
 @endsection
