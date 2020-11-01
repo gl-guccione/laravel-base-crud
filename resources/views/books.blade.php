263a1bf18e31768b835ae02065b38721951db1c9
gl-guccione <g.guccione3@gmail.com>
2020-11-01 23:22:51 +0100

    Added Fontawesome + google font + sass

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -4,21 +4,44 @@
 
 @section('mainContent')
 
-  <ul>
+  <main class="main_boxed">
 
-    @foreach ($books as $book)
+    <ul class="books__collection">
 
+      @foreach ($books as $book)
 
-    <li class="book">
-      <h2 class="book__title">{{$book->title}}</h2>
-      <img class="book__image" src="{{$book->image}}" alt="copertina libro - {{$book->title}}">
-      <small class="book__isbn">{{$book->isbn}}</small>
-      <a href="{{route('books.show', $book->id)}}">Ulteriori informazioni</a>
-    </li>
+        <li class="book">
 
-    @endforeach
+          <a href="{{route('books.show', $book->id)}}">
+            <img class="book__image" src="{{$book->image}}" alt="copertina libro - {{$book->title}}">
+          </a>
 
-  </ul>
+          <h2 class="book__title">{{$book->title}}, {{$book->author}}</h2>
+
+          <h3 class="book__genre">{{$book->genre}}</h3>
+
+          <h4 class="book__isbn"><i class="fas fa-barcode"></i> {{$book->isbn}}</h4>
+
+        </li>
+
+      @endforeach
+
+      <li class="book">
+
+        <a href="{{route('books.create')}}">
+          <div class="book__image">
+            <i class="fas fa-plus-square"></i>
+          </div>
+        </a>
+
+        <h2 class="book__add">Aggiungi un libro</h2>
+
+      </li>
+
+    </ul>
+
+  </main>
+
 
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,11 @@
-<header>
+<header class="header">
 
-  <a href="{{route('books.index')}} "> Home </a>
+  <a class="header__home" href="{{route('books.index')}}">
+    Home
+  </a>
 
-  <a href="{{route('books.create')}}">+</a>
+  <a class="header__add" href="{{route('books.create')}}">
+    <i class="fas fa-plus"></i>
+  </a>
 
 </header>
