d3d3d350824287ac18030f86d9a717ba3a62a22e
gl-guccione <g.guccione3@gmail.com>
2020-11-02 00:09:38 +0100

    Added sass for edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,30 +4,69 @@
 
 @section('mainContent')
 
-  <form action="{{route('books.update', $book->id)}}" method="POST">
-    @csrf
-    @method('PUT')
+  <main class="main_boxed">
 
-    <input type="text" name="title" placeholder="title" value="{{$book->title}}" required>
-    <input type="text" name="image" placeholder="image" value="{{$book->image}}" required>
-    <input type="text" name="isbn" placeholder="isbn" value="{{$book->isbn}}" required>
-    <input type="text" name="author" placeholder="author" value="{{$book->author}}" required>
-    <input type="text" name="genre" placeholder="genre" value="{{$book->genre}}" required>
-    <input type="date" name="year" placeholder="year" value="{{$book->year}}" required>
-    <input type="number" name="pages" placeholder="pages" value="{{$book->pages}}" required>
+    <form action="{{route('books.update', $book->id)}}" method="POST">
+      @csrf
+      @method('PUT')
 
-    <input type="submit" value="Aggiorna i dati">
+      <div class="book-show">
 
-  </form>
+        <div class="book-show__left">
+          <img class="book-show__image" src="{{$book->image}}" alt="immagine mancante">
+          <div>
+            <h5 class="input-label">URL immagine:</h5>
+            <input class="input-full" type="text" name="image" placeholder="image" value="{{$book->image}}" required>
+          </div>
+        </div>
 
-  @if ($errors->any())
-      <div>
-          <ul>
-              @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-              @endforeach
-          </ul>
+        <div class="book-show__right">
+
+          <div>
+            <h5 class="input-label">Titolo:</h5>
+            <input class="input-full" type="text" name="title" placeholder="title" value="{{$book->title}}" required>
+          </div>
+          <div>
+            <h5 class="input-label">ISBN:</h5>
+            <input class="input-full" type="text" name="isbn" placeholder="isbn" value="{{$book->isbn}}" required>
+          </div>
+          <div>
+            <h5 class="input-label">Autore:</h5>
+            <input class="input-full" type="text" name="author" placeholder="author" value="{{$book->author}}" required>
+          </div>
+          <div>
+            <h5 class="input-label">Genere:</h5>
+            <input class="input-full" type="text" name="genre" placeholder="genre" value="{{$book->genre}}" required>
+          </div>
+          <div>
+            <h5 class="input-label">Anno di pubblicazione:</h5>
+            <input class="input-full" type="date" name="year" placeholder="year" value="{{$book->year}}" required>
+          </div>
+          <div>
+            <h5 class="input-label">Numero di pagine:</h5>
+            <input class="input-full" type="number" name="pages" placeholder="pages" value="{{$book->pages}}" required>
+          </div>
+
+        </div>
+
       </div>
-  @endif
+
+
+      <input class="btn" type="submit" value="Aggiorna i dati">
+
+    </form>
+
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+  </main>
+
 
 @endsection
